e07078d52e7c6e96764584cd646fef8cf454c12b
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2026-01-27 21:57:11 +0900

    shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,114 @@
+import { toast } from "sonner";
 import "./App.css";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+import { Toaster } from "./components/ui/sonner";
+import { Textarea } from "./components/ui/textarea";
+import { cn } from "./lib/utils";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./components/ui/carousel";
+import { Popover } from "./components/ui/popover";
+import { PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import { DialogHeader } from "./components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./components/ui/alert-dialog";
 
 function App() {
+  const isActive = true;
   return (
-    <div>
-      {/* 타이포 그래피 */}
-      <div className="text-xs text-red-500">text-xs</div>
-      <div className="text-sm">text-sm</div>
-      <div className="text-lg font-bold">text-lg</div>
-      <div className="text-xl font-black">text-xl</div>
-      <div className="text-2xl font-extrabold">text-2xl</div>
-      <div className="text-[13px] text-[rgb(100,300,20)]">text-13px</div>
+    <div className="p-5">
+      <AlertDialog>
+        <AlertDialogTrigger>Open Alert Dialog</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogTitle>title</AlertDialogTitle>
+          <div>안녕!</div>
+          <AlertDialogAction
+            onClick={() => {
+              console.log("Action");
+            }}
+          >
+            Action
+          </AlertDialogAction>
+          <AlertDialogCancel
+            onClick={() => {
+              console.log("Cancel");
+            }}
+          >
+            Cancel
+          </AlertDialogCancel>
+        </AlertDialogContent>
+      </AlertDialog>
 
-      {/* 백그라운드 컬러 */}
-      <div className="bg-amber-500">amber-500</div>
+      <Dialog>
+        <DialogTrigger>Open Dialog</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogContent>Title</DialogContent>
+            <DialogDescription>Description</DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button>open</Button>
+        </PopoverTrigger>
+        <PopoverContent>Content!</PopoverContent>
+      </Popover>
 
-      {/* 사이즈 */}
-      <div className="h-20 w-full bg-blue-500">box</div>
+      <Carousel className="mx-10">
+        <CarouselContent>
+          <CarouselItem className="basic-1/3">1</CarouselItem>
+          <CarouselItem>2</CarouselItem>
+          <CarouselItem>3</CarouselItem>
+          <CarouselItem>4</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
 
-      {/* 여백 */}
-      <div className="m-5 h-50 w-50 bg-red-400 px-5 py-5">
-        <div className="h-full w-full bg-blue-400"></div>
+      <Toaster />
+      <Textarea />
+      <Input placeholder="... 입력" />
+
+      <Button
+        onClick={() => {
+          toast("토스트 메시지", {
+            position: "top-center",
+          });
+        }}
+      >
+        버튼!
+      </Button>
+      <Button variant={"destructive"}>버튼!</Button>
+      <Button variant={"ghost"}>버튼!</Button>
+      <Button variant={"link"}>버튼!</Button>
+      <Button variant={"outline"}>버튼!</Button>
+      <Button variant={"secondary"}>버튼!</Button>
+
+      <div className={cn(isActive ? "text-green-500" : "text-red-500")}>
+        isActive
       </div>
 
-      {/* 보더 */}
-      <div className="mx-5 border-x-2 border-y-2 border-red-500">border</div>
-      <div className="mx-5 rounded-2xl border-t-2 border-b-2">border</div>
-
-      {/* 플레스 컨테이너 */}
-      <div className="flex items-start justify-evenly">
-        <div className="h-10 w-10 flex-1 border">1</div>
-        <div className="h-20 w-10 border">2</div>
-        <div className="h-30 w-10 border">3</div>
-        <div className="h-40 w-10 border">4</div>
-      </div>
+      <div className="text-primary">Primary</div>
+      <div className="text-muted">Muted</div>
+      <div className="text-destructive">Destructive</div>
     </div>
   );
 }
